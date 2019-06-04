304132154b54721e53619296469fdab7f9b62537
Gergely Kocsis <koger23@gmail.com>
2019-06-04 20:28:58 +0200

    feat(ILogger): create interface

---
+++ src/main/java/com/gfa/programmersfoxclub/service/ILogger.java
@@ -0,0 +1,23 @@
+package com.gfa.programmersfoxclub.service;
+
+import com.gfa.programmersfoxclub.model.nutrition.Nutrition;
+
+import java.util.List;
+
+public interface ILogger {
+  List<String> getLastFiveActionList();
+
+  void setLastFiveActionList(List<String> lastFiveActionList);
+
+  void saveLoginAction();
+
+  void saveNutritionChange(Nutrition nutrition, String before, String after);
+
+  void saveTrickAction(String trick);
+
+  List<String> getActionList();
+
+  void setActionList(List<String> actionList);
+
+  List<String> getLastFiveAction();
+}
