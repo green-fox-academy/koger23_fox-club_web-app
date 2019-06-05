f66d1c18f45051f1863206bf45edc33169eab370
Gergely Kocsis <koger23@gmail.com>
2019-06-05 09:06:02 +0200

    feat(HomeController): add action history and nutritionstore endpoints

--- src/main/java/com/gfa/programmersfoxclub/controller/HomeController.java
+++ src/main/java/com/gfa/programmersfoxclub/controller/HomeController.java
@@ -1,22 +1,37 @@
 package com.gfa.programmersfoxclub.controller;
 
 
+import com.gfa.programmersfoxclub.model.character.Fox;
+import com.gfa.programmersfoxclub.model.trick.Trick;
 import com.gfa.programmersfoxclub.model.user.User;
-import com.gfa.programmersfoxclub.service.IFoxService;
-import com.gfa.programmersfoxclub.service.IUserService;
+import com.gfa.programmersfoxclub.service.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@Getter
+@Setter
 public class HomeController {
   private IFoxService foxService;
   private IUserService userService;
+  private ILogger logger;
+  private ISessionService sessionService;
+  private ITrickService trickRepository;
+  private INutritionService nutritionService;
 
-  public HomeController(IFoxService foxService, IUserService userService) {
+  public HomeController(IFoxService foxService, IUserService userService, ILogger logger, ISessionService sessionService, ITrickService trickRepository, INutritionService nutritionService) {
     this.foxService = foxService;
     this.userService = userService;
+    this.logger = logger;
+    this.sessionService = sessionService;
+    this.trickRepository = trickRepository;
+    this.nutritionService = nutritionService;
   }
 
   @GetMapping("/")
@@ -27,6 +42,41 @@ public class HomeController {
     }
     model.addAttribute("fox", foxService.findFoxByOwner(userService.getLoggedInUser()));
     model.addAttribute("user", userService.getLoggedInUser());
+    model.addAttribute("actionHistoryLogger", logger);
+    sessionService.updateFoxAndNutrition();
     return "index";
+  }
+
+  @GetMapping("/actionhistory")
+  public String actionHistory(Model model) {
+    if (userService.getLoggedInUser() == null) {
+      return "redirect:/login";
+    }
+    model.addAttribute("activeFoxIndex", foxService.findFoxByOwner(userService.getLoggedInUser()).getId());
+    model.addAttribute("fox", foxService.findFoxByOwner(userService.getLoggedInUser()));
+    model.addAttribute("actionHistoryLogger", logger);
+    return "actionhistory";
+  }
+
+  @GetMapping("/trickcenter")
+  public String trickCenter(Model model) {
+    if (userService.getLoggedInUser() == null) {
+      return "redirect:/login";
+    }
+    model.addAttribute("fox", foxService.findFoxByOwner(userService.getLoggedInUser()));
+    model.addAttribute("activeFoxIndex", foxService.findFoxByOwner(userService.getLoggedInUser()).getId());
+    model.addAttribute("tricklist", trickRepository.findAll());
+    return "trickcenter";
+  }
+
+  @PostMapping("/tricksave")
+  public String saveTrick(Model model, @RequestParam("trick") Trick trick) {
+    Fox fox = foxService.findFoxByOwner(userService.getLoggedInUser());
+    fox.getTrick_list().add(trick);
+    logger.saveTrickAction(trick.getName());
+    foxService.save(fox);
+    model.addAttribute("activeFoxIndex", fox.getId());
+    model.addAttribute("fox", fox);
+    return "redirect:/";
   }
 }
