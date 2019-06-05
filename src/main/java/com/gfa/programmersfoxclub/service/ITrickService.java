b7db4b5f30aecedeab276faaa525d089f0386835
Gergely Kocsis <koger23@gmail.com>
2019-06-05 08:56:54 +0200

    feat(ITrickService): create interface to handle Tricks

---
+++ src/main/java/com/gfa/programmersfoxclub/service/ITrickService.java
@@ -0,0 +1,9 @@
+package com.gfa.programmersfoxclub.service;
+
+import com.gfa.programmersfoxclub.model.trick.Trick;
+
+import java.util.List;
+
+public interface ITrickService {
+  List<Trick> findAll();
+}
