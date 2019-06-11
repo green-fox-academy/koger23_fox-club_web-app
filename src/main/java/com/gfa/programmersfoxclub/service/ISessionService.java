d2acf9986272da7e0751a4d6f902432d480623c7
Gergely Kocsis <koger23@gmail.com>
2019-06-11 19:35:05 +0200

    refactor(ISessionService): optimize imports

--- src/main/java/com/gfa/programmersfoxclub/service/ISessionService.java
+++ src/main/java/com/gfa/programmersfoxclub/service/ISessionService.java
@@ -1,7 +1,5 @@
 package com.gfa.programmersfoxclub.service;
 
-import com.gfa.programmersfoxclub.controller.HomeController;
-
 public interface ISessionService {
   void saveNutrition(String food, String drink);
 
