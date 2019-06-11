ff4b12fbab1c18ecc9657594db5ac3283a810a1d
Gergely Kocsis <koger23@gmail.com>
2019-06-11 15:57:52 +0200

    feat(NutirtionRepository): add new method to search

--- src/main/java/com/gfa/programmersfoxclub/repository/NutritionRepository.java
+++ src/main/java/com/gfa/programmersfoxclub/repository/NutritionRepository.java
@@ -1,8 +1,16 @@
 package com.gfa.programmersfoxclub.repository;
 
+import com.gfa.programmersfoxclub.model.nutrition.Drink;
+import com.gfa.programmersfoxclub.model.nutrition.Food;
 import com.gfa.programmersfoxclub.model.nutrition.Nutrition;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface NutritionRepository extends CrudRepository<Nutrition, Long> {
   boolean findByNameIsNotLike(String name);
+  Drink findDrinkById(long id);
+  Food findFoodById(long id);
+  List<Food> findAllFood();
+  List<Drink> findAllDrink();
 }
