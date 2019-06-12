4b045c648dc3397566d5bc242b96a114cde6c3d2
Gergely Kocsis <koger23@gmail.com>
2019-06-12 13:51:51 +0200

    feat(HealthServiceImp): work out heal()

--- src/main/java/com/gfa/programmersfoxclub/service/HealthServiceImp.java
+++ src/main/java/com/gfa/programmersfoxclub/service/HealthServiceImp.java
@@ -5,12 +5,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HealthServiceImp implements HealthService {
+  private FoxService foxService;
+
+  public HealthServiceImp(FoxService foxService) {
+    this.foxService = foxService;
+  }
+
   public void healTotally(Fox fox) {
     fox.setHealthPoints(fox.getMAX_HEALTH());
   }
 
-  public void heal(Fox fox, int healBy) {
-    fox.setHealthPoints(fox.getHealthPoints() + healBy);
+  public boolean heal(Fox fox) {
+    if (fox.getFood().getLevel() > 0 && fox.getDrink().getLevel() > 0 && fox.getHealthPoints() < fox.getMAX_HEALTH() && fox.isAlive()) {
+      fox.setHealthPoints(fox.getMAX_HEALTH());
+      foxService.save(fox);
+      return true;
+    }
+    return false;
   }
 
   public void harm(Fox fox, int damage) {
